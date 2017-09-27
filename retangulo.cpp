36b361e26a137c80c951b7faeae0279b94d7d2cd
HemersonRafael <rafaelpontes1995@gmail.com>
2017-09-27 07:37:32 -0300

    Update retangulo.cpp

--- retangulo.cpp
+++ retangulo.cpp
@@ -1,8 +1,13 @@
 #include "retangulo.h"
 
 Retangulo::Retangulo(float _x, float _y, float _largura, float _altura){
+    _x = x;
+    _y = y;
+    _largura = largura;
+    _altura = altura;
     Poligono::setVertice(_x,_y);
-    Poligono::setVertice(_x+_largura,_y);
+    Poligono::setVertice(_x,_y-_altura);
+    Poligono::setVertice(_x+_largura,_y-_altura;
     Poligono::setVertice(_x+_largura,_y+_altura);
-    Poligono::setVertice(_x,_y+_altura);
+    
 }
